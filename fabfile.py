f24c337de0cbe25949e6b5aac471ee9a479dd7c1
Sascha <sd@jackopaw.com>
2017-06-22 13:01:57 +0200

    try again

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,7 @@ from os import path
 env.domain = 'sila.network'
 env.ssl_email = 'sascha.dobblaere@gmail.com'
 env.rolefs = {
-	'web': ['www.sila.network'],
+	'web': ['www.sila.network',],
 	'mail': [],
 	'feature':[],
 }
