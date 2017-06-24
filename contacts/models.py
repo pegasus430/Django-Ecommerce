9f9391f6b62006fde1cb12fbe43c4a708cf18021
Sascha <sd@jackopaw.com>
2017-06-24 16:52:41 +0200

    more detail in contacts

--- contacts/models.py
+++ contacts/models.py
@@ -8,9 +8,14 @@ from django.db import models
 
 class Supplier(models.Model):
     business_name = models.CharField(max_length=100)
-    contact_name = models.CharField(max_length=100)
-    contact_phone = models.CharField(max_length=100)
-    contact_email = models.CharField(max_length=100)
+    contact_name = models.CharField(max_length=100, blank=True, null=True)
+    contact_phone = models.CharField(max_length=100, blank=True, null=True)
+    contact_mobile = models.CharField(max_length=100, blank=True, null=True)
+    contact_email = models.CharField(max_length=100, blank=True, null=True)
+    contact_address = models.CharField(max_length=100, blank=True, null=True)
+    contact_city = models.CharField(max_length=100, blank=True, null=True) 
+    contact_postcode = models.CharField(max_length=100, blank=True, null=True)
+    contact_country = models.CharField(max_length=100, blank=True, null=True)
 
     def __unicode__(self):
         return self.business_name
