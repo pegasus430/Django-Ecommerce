d0eb2f1e7e1dc4e1534884e8104c00c7cff433e4
Sascha <sd@jackopaw.com>
2017-06-26 21:39:52 +0200

    add stock-info

--- inventory/admin.py
+++ inventory/admin.py
@@ -68,6 +68,7 @@ class MaterialAdmin(DefaultAdmin):
     list_display = ['name', 'sku_supplier', 'supplier', 'cost_per_usage_unit']
     list_filter = ['supplier', 'mat_type']
     search_fields = ['name', 'supplier__business_name']
+    readonly_fields = ['materials_on_stock']
     inlines = [StockLocationItemInline]
 
 class ProductModelAdmin(admin.ModelAdmin):
