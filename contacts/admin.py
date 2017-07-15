0a0c76c6801eee67dfdf83814fb1a7325d95895f
Sascha <sd@jackopaw.com>
2017-07-15 07:55:59 +0200

    adding app;

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Relation
 
 class RelationAdmin(admin.ModelAdmin):
-	list_display = ('business_name', 'contact_phone')
+	list_display = ('business_name', 'contact_name', 'contact_phone')
 
 admin.site.register(Relation, RelationAdmin)
