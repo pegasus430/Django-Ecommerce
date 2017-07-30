5d2fcc13c3187bf2cb9a2096a8ef5130cc200e31
Sascha <sd@jackopaw.com>
2017-07-30 07:05:59 +0200

    add filters

--- sales/admin.py
+++ sales/admin.py
@@ -26,6 +26,8 @@ class PriceListAdmin(DefaultAdmin):
 
 class PriceListItemAdmin(DefaultAdmin):
 	list_display = ['__unicode__', 'price_list', 'rrp', 'per_1', 'per_6', 'per_12', 'per_48']
+	list_filter = ['price_list']
+	search_fields = ['__unicode__']
 
 admin.site.register(SalesOrder, SalesOrderAdmin)
 admin.site.register(SalesOrderProduct, SalesOrderProductAdmin)
