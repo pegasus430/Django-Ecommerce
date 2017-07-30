7ff20d5c5a105ac7cebea49d7fe363f89fdea954
Sascha <sd@jackopaw.com>
2017-07-30 07:11:57 +0200

    add filters

--- sales/admin.py
+++ sales/admin.py
@@ -7,27 +7,33 @@ from .models import *
 ### Inlines ###
 ###############
 class SalesOrderProductInline(DefaultInline):
-	model=SalesOrderProduct
+    model=SalesOrderProduct
 
 class PriceListItemInline(DefaultInline):
-	model=PriceListItem
+    model=PriceListItem
 
 #####################
 ### Custom Admins ###
 #####################
 class SalesOrderAdmin(DefaultAdmin):
-	inlines = [SalesOrderProductInline]
+    inlines = [SalesOrderProductInline]
 
 class SalesOrderProductAdmin(DefaultAdmin):
-	list_display = ['product', 'qty', 'unit_price']
+    list_display = ['product', 'qty', 'unit_price']
 
 class PriceListAdmin(DefaultAdmin):
-	inlines = [PriceListItemInline]
+    inlines = [PriceListItemInline]
 
 class PriceListItemAdmin(DefaultAdmin):
-	list_display = ['__unicode__', 'product__sku', 'price_list', 'rrp', 'per_1', 'per_6', 'per_12', 'per_48']
-	list_filter = ['price_list']
-	search_fields = ['__unicode__']
+    list_display = ['__unicode__', 'get_sku', 'price_list', 'rrp', 'per_1', 'per_6', 'per_12', 'per_48']
+    list_filter = ['price_list']
+    search_fields = ['__unicode__']
+
+    def get_sku(self, obj):
+        return obj.product.sku
+    # get_sku.admin_order_field  = 'SKU'  #Allows column order sorting
+    get_sku.short_description = 'SKU'  #Renames column head
+
 
 admin.site.register(SalesOrder, SalesOrderAdmin)
 admin.site.register(SalesOrderProduct, SalesOrderProductAdmin)
