70010e0b6006d8530fb6ba6f3c015fd29e508cd9
Sascha <sd@jackopaw.com>
2017-08-30 21:10:27 +0200

    add barcode label generation

--- inventory/labels.py
+++ inventory/labels.py
@@ -1,11 +1,11 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import mm
+from reportlab.graphics.barcode import eanbc
+from reportlab.graphics.shapes import Drawing 
+from reportlab.graphics import renderPDF
 
 from io import BytesIO
-
-import barcode
-from barcode.writer import ImageWriter
 
 
 def box_barcode_label_38x90(product):
@@ -16,14 +16,12 @@ def box_barcode_label_38x90(product):
     product_size = product.product_model.size
     product_sku = product.sku
 
-    EAN = barcode.get_barcode_class('ean13')
+    # EAN = barcode.get_barcode_class('ean13')
     buffer = BytesIO()
 
-    page_real_height = 33*mm  ## Cheating to fix layout, real value 38
-    page_real_width = 83*mm  ## Cheating to fix layout, real value 90
-    page_margin = 0*mm
-    page_height = page_real_height - page_margin
-    page_width = page_real_width - page_margin
+    page_real_height = 38*mm  ## Cheating to fix layout, real value 38
+    page_real_width = 90*mm  ## Cheating to fix layout, real value 90
+    page_margin = 5*mm
     line_height = 2.8*mm
     font_size = 8
 
@@ -32,20 +30,20 @@ def box_barcode_label_38x90(product):
 
     p.setLineWidth(.3)
     col1 = 0 
-    col2 = page_real_width / 2 + 10*mm
+    col2 = page_real_width / 2 + page_margin
 
-    ori_string_top_location = page_height
-    string_top_location = ori_string_top_location - 15*mm
-    
-    ## Left-col
-    ean = EAN(product_ean, writer=ImageWriter())
-    filename = ean.save('/tmp/barcode')
-    #p.drawImage(filename, 0*mm, 0*mm, mask='auto')
-    barcode_ratio = 38.0/71.0
-    barcode_height = 33*mm
-    barcode_width = barcode_height / barcode_ratio
-    p.drawImage(filename, -5*mm, -5*mm, width=barcode_width, height=barcode_height, mask='auto')
-    #p.drawImage(col1, string_top_location,'Fabric #: {}'.format(counter_id))
+    ori_string_top_location = page_real_height - page_margin
+
+    ## Left col
+    string_top_location = ori_string_top_location - 10*mm
+
+    ean = eanbc.Ean13BarcodeWidget(product_ean)
+    ean.height = page_real_height
+    ean.width = page_real_width / 2
+
+    d = Drawing()
+    d.add(ean)
+    renderPDF.draw(d, p, page_margin, page_margin)
 
     ## Right col info
     p.setFont('Helvetica', font_size)
