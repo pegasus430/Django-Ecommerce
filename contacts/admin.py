27d5846af6bb1a6b120a8725d27edac3c1cd5a02
Sascha <sd@jackopaw.com>
2017-09-10 10:10:52 +0200

    improve listing

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,6 +4,7 @@ from .models import Relation, RelationAddress, OwnAddress, Agent
 
 class RelationAdmin(admin.ModelAdmin):
 	list_display = ('business_name', 'contact_name', 'contact_phone')
+	list_filter = ['is_supplier', 'is_client', 'country', 'agent']
 
 
 class OwnAddressAdmin(admin.ModelAdmin):
