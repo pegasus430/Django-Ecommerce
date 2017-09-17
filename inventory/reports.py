66967147d9bc2718fc05d15ddcc79c2398421f24
Sascha <sd@jackopaw.com>
2017-09-17 16:53:08 +0200

    adding patterns

--- inventory/reports.py
+++ inventory/reports.py
@@ -366,7 +366,7 @@ def production_notes_for_umbrella_product(umbrella_product):
     for product in umbrella_product.product_set.all():
         for pattern in product.product_model.productmodelpattern_set.all():
             table_data.append([
-                product.size.short_size,
+                product.product_model.size.short_size,
                 pattern.name,
                 pattern.get_pattern_type_display(),
                 pattern.times_to_use,
