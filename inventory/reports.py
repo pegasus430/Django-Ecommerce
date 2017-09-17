bd6ed071b6efa1926c68d328aac5474d6057515e
Sascha <sd@jackopaw.com>
2017-09-17 17:03:23 +0200

    add date

--- inventory/reports.py
+++ inventory/reports.py
@@ -298,9 +298,8 @@ def production_notes_for_umbrella_product(umbrella_product):
     bullet = 'Bullet'
     text = 'BodyText'
     
-    document.add_text('Production notes for {}'.format(base_sku), title)
-    document.add_text('{}'.format(datetime.date.today().strftime("%d %B, %Y")), text)
-    
+    document.add_text('Production notes for {} on {}'.format(base_sku, 
+        datetime.date.today().strftime("%d %B, %Y")), title)    
 
     document.add_text('Product details', heading)
     document.add_text('Collection: {} {}'.format(collection, collection_number), bullet)
