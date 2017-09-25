c02e115eadbe7a3e6302321e584d28d5af270505
Sascha <sd@jackopaw.com>
2017-09-25 09:54:47 +0200

    remove old data

--- inventory/admin.py
+++ inventory/admin.py
@@ -115,11 +115,6 @@ class ProductModelPatternAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'product']
     list_filter = ['product__umbrella_product_model__number']
 
-    def get_umbrella_product_model_number(self, obj):
-        return obj.product.umbrella_product_model.number
-    get_umbrella_product_model_number.admin_order_field = 'Model Number'
-    get_umbrella_product_model_number.short_description= 'Model Number'
-
 
 class UmbrellaProductAdmin(admin.ModelAdmin):    
     list_display = ['__unicode__','base_sku', 'active', 'complete', 'get_umbrella_product_model_number', 'number_of_sizes']
