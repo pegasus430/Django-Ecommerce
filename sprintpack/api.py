534a406a998b442c2d93b176a95c7ac6560882f6
Sascha <sd@jackopaw.com>
2017-09-27 17:50:15 +0200

    first stab at SprintClient class

---
+++ sprintpack/api.py
@@ -0,0 +1,43 @@
+import requests
+import xmltodict
+
+class SprintClient:
+    def __init__(self, webshopcode):
+        self.webshopcode = webshopcode
+        self.url = 'http://ewms.sprintpack.be:1450/'
+        self.headers = {
+            'content-type': 'text/xml',
+            #'content-type': 'application/soap+xml',
+            # 'SoapAction': 'RequestOrderStatus'
+        }
+
+
+    def post(self, data, soapaction):
+        '''
+        Post the request to the sprintpack server, with the given webshopcode.
+        - headers will add the SoapAction
+        - data will be send raw
+        Return: response content xml, unprocessed
+        '''
+        headers = self.headers.copy()
+        headers['SoapAction'] = soapaction
+        data = '''
+        <?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+             xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+        <WebshopCode>{webshopcode}</WebshopCode>
+        {data}
+       </soap:Body>
+        </soap:Envelope>
+        '''.format(webshopcode=self.webshopcode, data=data)
+
+        response = requests.post(url,data=data,headers=headers)
+        converted_response = xmltodict.parse(response.content)
+        return converted_response
+
+
+    def create_order(self, order_dict):
+        '''dict with data to create an order'''
+        self.post(converted_order_dict, 'CreateOrder')
