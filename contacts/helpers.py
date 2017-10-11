f999b611a183d8e1d5f41c892a109917c2d04a7e
Sascha <sd@jackopaw.com>
2017-10-11 15:39:56 +0200

    fix label name

--- contacts/helpers.py
+++ contacts/helpers.py
@@ -10,13 +10,20 @@ def print_address_label_admin(addresses):
     label_data = {}
     for address in addresses:
         filename = u'{}.pdf'.format(address.business_name)
+        if address.contact_first_name and address.contact_name:
+            name = u'{address.contact_first_name} {address.contact_name}'.format(address=address)
+        elif address.contact_first_name:
+            name = u'{address.contact_first_name}'.format(address=address)
+        else:
+            name = u'{address.contact_name}'.format(address=address)
+
         text = u'''
             {address.business_name}
-            {address.contact_first_name} {address.contact_name}
+            {name}
             {address.address1} {address.address2}
             {address.postcode} {address.city}
             {country}
-            '''.replace('\n', '<br></br>').format(address=address, country=address.get_country_display())
+            '''.replace('\n', '<br></br>').format(address=address, country=address.get_country_display(), name=name)
         label_data[filename] = simple_label_38x90(text)
 
     return dynamic_file_httpresponse(label_data, u'address_labels')
