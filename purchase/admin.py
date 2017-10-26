cbd21b416c1c7ce76ffd097e0649da5e954d9fd0
Sascha <sd@jackopaw.com>
2017-10-26 09:31:09 +0200

    add created_at to admin

--- purchase/admin.py
+++ purchase/admin.py
@@ -26,7 +26,7 @@ class DeliveryAttachmentInline(DefaultInline):
 #####################
 class PurchaseOrderAdmin(DefaultAdmin):
     readonly_fields = ['order_value', 'created_at', 'updated_at', 'id']
-    list_display = ['__unicode__', 'status', 'est_delivery']
+    list_display = ['__unicode__', 'status','created_at', 'est_delivery']
     list_filter = ['status', 'supplier', 'est_delivery']
     inlines = [PurchaseOrderItemInline, PurchaseOrderConfirmationAttachmentInline]
     actions = [print_purchase_order_report, mark_as_awaiting_for_confirmation, mark_as_awaiting_delivery]
