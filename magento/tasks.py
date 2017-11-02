b5fa80c6bb1d202c0d4c60bbe36180a69b7f0d99
Sascha <sd@jackopaw.com>
2017-11-02 14:01:25 +0100

    upload product sizes

--- magento/tasks.py
+++ magento/tasks.py
@@ -1,2 +1,16 @@
 from huey import crontab
 from huey.contrib.djhuey import db_task, db_periodic_task
+
+import api
+from inventory.models import Product
+
+
+@db_task()
+def update_simple_product_sizes():
+	mag = api.MagentoServer()
+	for sku in mag.get_product_list():
+		try:
+			product = Product.objects.get(sku=sku)
+			mag.product_update({'sku': sku, 'size_chart': product.product_model.size_description})
+		except Product.DoesNotExist:
+			pass
