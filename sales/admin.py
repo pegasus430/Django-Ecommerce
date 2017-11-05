5ed0c770748effdce2756acdf9e124c8e7344ebe
Sascha <sd@jackopaw.com>
2017-11-05 19:36:10 +0100

    remove unused status field

--- sales/admin.py
+++ sales/admin.py
@@ -24,7 +24,6 @@ class SalesOrderNoteInline(DefaultExpandedInline):
     model = SalesOrderNote
 
 class PriceListItemInline(DefaultInline):
-    readonly_fields = ['active']
     model=PriceListItem
 
 class SalesOrderDeliveryItemInline(DefaultInline):
