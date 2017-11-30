8a6a066c0959a8c34178bf085d06ebc128354f45
Sascha <sd@jackopaw.com>
2017-11-30 07:43:50 +0100

    add export_datafile_for_customer

--- contacts/helpers.py
+++ contacts/helpers.py
@@ -1,6 +1,9 @@
 from printing_tools.labels import simple_label_38x90
 from defaults.helpers import dynamic_file_httpresponse
 
+from sales.helpers import export_product_datafile
+from sales.models import PriceList 
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -22,14 +25,28 @@ def print_address_label_admin(addresses):
             {address.address1} {address.address2}
             {address.postcode} {address.city}
             {country}
-            '''.replace('\n', '<br></br>').format(address=address, country=address.get_country_display(), name=name)
+            '''.replace('\n', '<br></br>').format(address=address, 
+                country=address.get_country_display(), name=name)
         label_data[filename] = simple_label_38x90(text)
 
     return dynamic_file_httpresponse(label_data, u'address_labels')
 
+
+def export_datafile_for_customer_admin(relations):
+    exported_files = {}
+    for relation in relations:
+        pricelist = PriceList.objects.get(currency=relation.currency, 
+            customer_type=relation.customer_type)
+        exported_files['{}.csv'.format(relation)] = export_product_datafile(pricelist)
+
+    return exported_files
 
 
 ### admin helpers ###
 def print_address_label(modeladmin, request, queryset):
     return print_address_label_admin(queryset)
 print_address_label.short_description = 'Print address labels'
+
+def export_datafile_for_customer(modeladmin, request, queryset):
+    return export_datafile_for_customer_admin(queryset)
+export_datafile_for_customer.short_description = 'Print product data-files'
