cb8be0b8362d580a26bac9c23c13cbeebab527c4
Sascha <sd@jackopaw.com>
2017-11-30 07:45:04 +0100

    remove new function, not importing - sort out

--- contacts/helpers.py
+++ contacts/helpers.py
@@ -1,8 +1,8 @@
 from printing_tools.labels import simple_label_38x90
 from defaults.helpers import dynamic_file_httpresponse
 
-from sales.helpers import export_product_datafile
-from sales.models import PriceList 
+# from sales.helpers import export_product_datafile
+# from sales.models import PriceList 
 
 import logging
 logger = logging.getLogger(__name__)
@@ -32,14 +32,14 @@ def print_address_label_admin(addresses):
     return dynamic_file_httpresponse(label_data, u'address_labels')
 
 
-def export_datafile_for_customer_admin(relations):
-    exported_files = {}
-    for relation in relations:
-        pricelist = PriceList.objects.get(currency=relation.currency, 
-            customer_type=relation.customer_type)
-        exported_files['{}.csv'.format(relation)] = export_product_datafile(pricelist)
+# def export_datafile_for_customer_admin(relations):
+#     exported_files = {}
+#     for relation in relations:
+#         pricelist = PriceList.objects.get(currency=relation.currency, 
+#             customer_type=relation.customer_type)
+#         exported_files['{}.csv'.format(relation)] = export_product_datafile(pricelist)
 
-    return exported_files
+#     return exported_files
 
 
 ### admin helpers ###
@@ -47,6 +47,6 @@ def print_address_label(modeladmin, request, queryset):
     return print_address_label_admin(queryset)
 print_address_label.short_description = 'Print address labels'
 
-def export_datafile_for_customer(modeladmin, request, queryset):
-    return export_datafile_for_customer_admin(queryset)
-export_datafile_for_customer.short_description = 'Print product data-files'
+# def export_datafile_for_customer(modeladmin, request, queryset):
+#     return export_datafile_for_customer_admin(queryset)
+# export_datafile_for_customer.short_description = 'Print product data-files'
