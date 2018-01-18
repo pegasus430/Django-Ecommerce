a323dd49858b3a92d40401d898bc541c817a1951
Sascha <sd@jackopaw.com>
2018-01-18 16:25:39 +0100

    unicode strings

--- defaults/helpers.py
+++ defaults/helpers.py
@@ -28,7 +28,7 @@ def multiple_files_to_zip_httpresponse(files, zip_filename):
             zf.writestr(k, v)
     
     response = HttpResponse(outfile.getvalue(), content_type="application/octet-stream")
-    response['Content-Disposition'] = 'attachment; filename={}.zip'.format(zip_filename)
+    response['Content-Disposition'] = u'attachment; filename={}.zip'.format(zip_filename)
     return response
 
 
@@ -51,7 +51,7 @@ def single_file_httpresponse(filedata, filename):
         raise Exception('Content-Type for extension {} unkown'.format(extension))
 
     response = HttpResponse(content_type=content_type)
-    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+    response['Content-Disposition'] = u'attachment; filename="{}"'.format(filename)
 
     response.write(filedata)
     return response
