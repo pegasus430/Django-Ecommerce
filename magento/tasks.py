72be5c6de29b2a3fead304dcdd88422917d388c5
Sascha <sd@jackopaw.com>
2018-02-23 17:51:25 +0100

    only update pics when dealing with configs

--- magento/tasks.py
+++ magento/tasks.py
@@ -46,8 +46,8 @@ def update_or_create_product(magento, price_list_item):
         logger.error('Failed to update product {} with status {}'.format(sku, e))
 
 
-    p_type, attribute_set, sku, data = compiler.config_item()
     # Step 2, do config item if previous simple item is the last one.
+    p_type, attribute_set, sku, data = compiler.config_item()
     if compiler.simple_item_last():    
         try:
             logger.debug('Trying to create config item {}'.format(sku))
@@ -61,28 +61,28 @@ def update_or_create_product(magento, price_list_item):
             logger.error('Failed to update config product {} with status {}'.format(sku, e))
 
 
-    # Step 3, upload new pictures if needed
-    logger.debug('Comparing known images for {}'.format(sku))
+    # Step 3, upload new pictures if needed    
+    if compiler.simple_item_last(): 
+        logger.debug('Comparing known images for {}'.format(sku))
+        try:
+            magento.get_product_info(sku)
+            magento_image_name_list = [extract_filename(i) for \
+                i in magento.product_image_list(sku)]
+            
+            sila_image_object_list = compiler.umbrella_product.umbrellaproductimage_set.all()
+            sila_images_to_upload = [i.image.path for i in sila_image_object_list\
+                if extract_filename(i.image.url) not in magento_image_name_list]
+            
+            logger.debug('Uploading new images for {} ({})'.format(sku,sila_images_to_upload))
+            magento.product_image_create(sku, sila_images_to_upload)
+            # for i in sila_image_object_list:
+            #     if extract_filename(i.image.url) not in magento_image_name_list:
+            #         sila_images_to_upload.append(i.image.path)
 
-    try:
-        magento.get_product_info(sku)
-        magento_image_name_list = [extract_filename(i) for \
-            i in magento.product_image_list(sku)]
+        except ProductDoesNotExist:
+            logger.info('Unknown product {}, skipping images'.format(sku))
+            pass
         
-        sila_image_object_list = compiler.umbrella_product.umbrellaproductimage_set.all()
-        sila_images_to_upload = [i.image.path for i in sila_image_object_list\
-            if extract_filename(i.image.url) not in magento_image_name_list]
-        
-        logger.debug('Uploading new images for {} ({})'.format(sku,sila_images_to_upload))
-        magento.product_image_create(sku, sila_images_to_upload)
-        # for i in sila_image_object_list:
-        #     if extract_filename(i.image.url) not in magento_image_name_list:
-        #         sila_images_to_upload.append(i.image.path)
-
-    except ProductDoesNotExist:
-        logger.info('Unknown product {}, skipping images'.format(sku))
-        pass
-    
 
 
 def update_or_create_products():
