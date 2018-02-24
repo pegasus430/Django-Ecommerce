b3121702d84bab86e0e1143e08c2265279bd553b
Sascha <sd@jackopaw.com>
2018-02-24 08:02:15 +0100

    try to compare image data rather then names.  Magento changes them.

--- magento/tasks.py
+++ magento/tasks.py
@@ -66,13 +66,22 @@ def update_or_create_product(magento, price_list_item):
         logger.debug('Comparing known images for {}'.format(sku))
         try:
             magento.get_product_info(sku)
-            magento_image_name_list = [extract_filename(i) for \
+            magento_images = [urllib2.urlopen(i).read() for \
                 i in magento.product_image_list(sku)]
             
             sila_image_object_list = compiler.umbrella_product.umbrellaproductimage_set.all()
-            sila_images_to_upload = [i.image.path for i in sila_image_object_list\
-                if extract_filename(i.image.url) not in magento_image_name_list]
-            
+            sila_images_to_upload = []
+
+            for sila_img in sila_image_object_list:
+                with open(sila_img.image.path) as sila_img_data:
+                    for mag_image_data in magento_images:
+                        match = False
+                        if mag_image_data == sila_img_data:
+                            match = True
+
+                if not match:
+                    sila_images_to_upload.append(sila_img.image.path)
+
             logger.debug('Uploading new images for {} ({})'.format(sku,sila_images_to_upload))
             magento.product_image_create(sku, sila_images_to_upload)
             # for i in sila_image_object_list:
